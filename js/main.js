bcca2d533b1abdf79601610a35335f2a4d8d240a
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-15 01:38:02 +0200

    fixed code using more vueJS

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ createApp({
     data() {
         return {
             title: 'ToDoList di Giacomo',
-            toDo: '',
+            // toDo: '',
+            toDo: [],
+            newToDo: '',
         }
     },
     methods: {
-        createLi(toDo, idLista, idInput) {
-            const newList = document.createElement('li')
-            newList.innerHTML = toDo
-            document.getElementById(idLista).append(newList)
-            document.getElementById(idInput).value = ''
+        // createLi(toDo, idLista, idInput) {
+        //     const newList = document.createElement('li')
+        //     newList.innerHTML = toDo
+        //     document.getElementById(idLista).append(newList)
+        //     document.getElementById(idInput).value = ''
+        // }
+        addToDo(toDo) {
+            this.toDo.push(toDo)
+            console.log(this.toDo)
         }
     }
 }).mount('#app')
